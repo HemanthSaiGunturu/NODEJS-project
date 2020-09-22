80918313c70ec76f7a5459b6e281d45b8ab6d75b
HemanthSaiGunturu <gunturu.h@northeastern.edu>
2020-09-21 22:41:03 -0400

    checked api with server

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 
 
 const server = http.createServer((req,res)=>{
 
-    
-    res.end("This is from the server...!");
+    const pathName = req.url;
 
+    if (pathName === '/')
+    res.end("this is the default page");
+
+    if (pathName === '/product')
+    res.end("this is the product page");
+
+    if (pathName === '/api')
+    {
+        res.writeHead(200,{
+            'Content-type': 'application/json',
+            'My-header' : 'Hemanth Header'
+        });
+        res.end(data);
+    }
 
 });
 
