f38375942d7d1840ca0282809000af5250e3fc90
HemanthSaiGunturu <gunturu.h@northeastern.edu>
2020-09-22 00:35:19 -0400

    own modules

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-
+const replacetemplate = require('./modules/replacetemplate');
 
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
 
-const replacetemplate = (temp, product)=>{
-    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
-    output = output.replace(/{%IMAGE%}/g,product.image);
-    output = output.replace(/{%QUANTITY%}/g,product.quantity);
-    output = output.replace(/{%FROM%}/g,product.from);
-    output = output.replace(/{%PRICE%}/g,product.price);
-    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients);
-    output = output.replace(/{%PRODUCT_DESCRIPTION%}/g,product.description);
-    output = output.replace(/{%ID%}/g,product.id)
-
-    if(!product.organic)
-    output = output.replace(/{%NOTORGANIC%}/g,'not-organic');
-    return output;
-
-}
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
-
-
 
 
 
