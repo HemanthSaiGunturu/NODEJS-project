80dd2bfda1f15580de2ba3b390b65c214d49eb85
HemanthSaiGunturu <gunturu.h@northeastern.edu>
2020-09-22 22:48:18 -0400

    face 1 completed

--- index.js
+++ index.js
@@ -1,56 +1,63 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
 const replacetemplate = require('./modules/replacetemplate');
 
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
-
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
+console.log(slugify('Fresh Avacados', { lower: true }));
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
 
-    const {query, pathname}= url.parse(req.url,true);
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
-    if (pathname === '/' || pathname === '/overview')
-    {
-        res.writeHead(200,{'Content-type':'text/html'});
+    const cardsHtml = dataObj
+      .map((el) => replacetemplate(tempCard, el))
+      .join('');
 
-        const cardsHtml = dataObj.map(el => replacetemplate(tempCard, el) ).join('');
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+    res.end(output);
+  }
+  if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const product = dataObj[query.id];
+    const output = replacetemplate(tempProduct, product);
+    res.end(output);
 
-        const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
-        res.end(output);
+    console.log(query);
+  }
+  // res.end("this is the product page");
 
-
-    }
-    if (pathname === '/product')
-    {
-        res.writeHead(200,{'Content-type':'text/html'});
-        const product = dataObj[query.id];
-        const output = replacetemplate(tempProduct,product);
-        res.end(output);
-
-        console.log(query);
-
-    }
-    // res.end("this is the product page");
-
-    if (pathname === '/api')
-    {
-        res.writeHead(200,{
-            'Content-type': 'application/json',
-            'My-header' : 'Hemanth Header'
-        });
-        res.end(data);
-    }
-
+  if (pathname === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+      'My-header': 'Hemanth Header',
+    });
+    res.end(data);
+  }
+  // console.log('chceking nodemon');
 });
 
-server.listen(8000,'127.0.0.1',()=>{
-    console.log(`Server initiated successfully on PORT 8000`);
+server.listen(8000, '127.0.0.1', () => {
+  console.log(`Server initiated successfully on PORT 8000`);
 });
